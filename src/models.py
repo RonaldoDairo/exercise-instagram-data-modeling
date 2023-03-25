1a521df72ba53c36e965f7ef8cf3e86adfb52d0b
RonaldoDairo <programaciondairo@gmail.com>
2023-03-25 17:30:09 +0000

    agregando project

--- src/models.py
+++ src/models.py
@@ -8,27 +8,42 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class Media(Base):
+        __tablename__ = 'media'
+        ID = Column(Integer, primary_key=True)
+        type =Column(String(250))
+        url = Column(String(250))
+        post_id = Column(Integer)
+    
+class Comment(Base):
+        __tablename__ = 'comment'
+        ID = Column(Integer, primary_key=True)
+        comment_text = Column(String(250))
+        author_id = Column(Integer)
+        post_id = Column(Integer)
+        
+class Follower(Base):
+        __tablename__ = 'follower'
+        user_from_id = Column(Integer ,primary_key = True)  
+        user_to_id = Column(Integer)  
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
-
-    def to_dict(self):
-        return {}
-
+class Post(Base):
+        __tablename__ = 'post'
+        user_id = Column(Integer) 
+        ID = Column(Integer, primary_key=True, foreign_keys=[ForeignKey('comment.post_id'), ForeignKey('media.post_id')])
+        comment = relationship(Comment)
+    
+class User(Base):
+        __tablename__ = 'user'
+        ID = Column(Integer, primary_key=True )
+        user_name= Column(String(250), unique = True )
+        firstname = Column(String(250), ) 
+        lastname =  Column(String(250),)
+        email = Column(String(250), unique = True)
+        person_id = Column(Integer, ForeignKey('follower.user_from_id ,'),
+         ForeignKey('follower.user_to_id'), ForeignKey('comment.author_id'), ForeignKey('post.user_id') )
+        person = relationship(Follower)
+    
 ## Draw from SQLAlchemy base
 try:
     result = render_er(Base, 'diagram.png')
